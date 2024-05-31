7defa15581cefd51e5a8ac75c0f558a0e657dcdb
shadan <shadan@spinachexperience.design>
2024-05-31 21:05:03 +0530

    check deployment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app, origins=["*"])
 
 @app.route("/flask")
 def hello():
-    return "flask inside Docker thru github!!"
+    return "flask inside Docker thru github!! on ECS!!!"
 
 
 if __name__ == "__main__":
